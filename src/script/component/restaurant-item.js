1264f4384ca349abaa1e506f605c947e743cc820
sagurah <satyogag@gmail.com>
2023-04-05 23:02:15 +0700

    done

--- src/script/component/restaurant-item.js
+++ src/script/component/restaurant-item.js
@@ -21,27 +21,70 @@ class RestaurantItems extends HTMLElement{
         }
 
         .restaurant-info {
-          background-color: #dbf1e9;
+          background-image: radial-gradient(circle, #dbf1e9, #d6f1e7, #d1f2e6, #cbf2e4, #c6f2e2);
+          border: 2px solid black;
           padding: 24px;
           margin-top: 20px;
+          transition: transform .5s;          
+        }
+
+        .restaurant-info:hover{
+          transform: scale(1.2)
         }
          
         .restaurant-info > h2 {
-          font-weight: lighter;
+          font-weight: 700;
         }
          
-        .restaurant-info > p {
+        .restaurant-info > textarea {
+          font-family: Comfortaa, sans-serif;
+          width: 95%;
+          height: 100px;
+          background-color: white;
+          border: 1px solid black;
+          border-radius: 10px;
+          box-shadow: 0px 1.5px black;
           margin-top: 10px;
+          padding: 7px;
           overflow: hidden;
+          font-size: 15px;
           text-overflow: ellipsis;
           display: -webkit-box;
           -webkit-box-orient: vertical;
           -webkit-line-clamp: 10; /* number of lines to show */
+          overflow-y: scroll;
         }
+
+        .categories{
+            margin-top: 0;
+            display: flex;
+            flex-direction: row;
+            gap: 10px;
+        }
+
+        .categories > .sub-categories {
+            border: 2px solid black;
+            box-shadow: 0px 2px black;
+            border-radius: 20px;
+            padding: 8px;
+        }
+
+        .categories > .sub-categories > span{
+            font-weight: 700;
+        }
+
         </style>
         <div class="restaurant-info">\n
-        <h2> ${this._restaurant.name} </h2>\n
-        <p> ${this._restaurant.description} </p> 
+            <h2> ${this._restaurant.name} </h2>\n
+            <div class="categories">
+                <div class="sub-categories">
+                    <span>📍${this._restaurant.city}</span>
+                </div>
+                <div class="sub-categories">
+                    <span>⭐ ${this._restaurant.rating}</span>
+                </div>
+            </div>
+            <textarea readonly> ${this._restaurant.description} </textarea> 
         </div>`
         
     }
